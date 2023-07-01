418a009b741edda270641a7d64b30c2670eebfa3
chandrabosep <chandrabosep3112@gmail.com>
2023-07-02 01:21:15 +0530

    Added animation and worked on few more pages

--- src/pages/Marinda.jsx
+++ src/pages/Marinda.jsx
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Marinda.scss'
 import title from '../Assets/Title.svg'
 import menu from '../Assets/menu.svg'
 import img1 from '../Assets/img1.jpeg'
 import img2 from '../Assets/img2.WEBP'
+import img3 from '../Assets/img3.jpeg'
+import img4 from '../Assets/img4.jpeg'
+import img5 from '../Assets/img5.png'
+import img6 from '../Assets/img6.WEBP'
+import img7 from '../Assets/img7.jpeg'
+
+import { gsap } from 'gsap'
 
 
 const Marinda = () => {
+
+  const page1 = useRef();
+
+  useEffect(()=>{
+    const animate = ()=>{
+      const tl = gsap.timeline();
+
+      tl.to(page1.current,{
+        y:'100vh',
+        scale:0.6,
+        duration:0
+      })
+
+      tl.to(page1.current,{
+        y:'30vh',
+        duration:1,
+        delay:1
+      })
+      tl.to(page1.current,{
+        y:'0vh',
+        rotate:360,
+        scale:1,
+        duration:0.7
+      })
+    }
+    return animate
+  },[])
+
+
   return (
     <>
       <div className="main">
-        <div className="page1">
+        <div className="page1" ref={page1}>
           <div className="nav">
             <h5>Amsterdam, NL</h5>
             <img src={title} className='title' alt="title" />
@@ -40,6 +76,55 @@ const Marinda = () => {
           </div>
           <h1>MIRANDA</h1>
         </div>
+        <div className="page2">
+          <div className="p2_left">
+            <h2>
+              INTERACTIVE 
+              <span> ARTIST!</span>
+            </h2>
+            <img src={img3} alt="" />
+            <p>As a multidisciplinary freelancer, I'm passionate about creating iconic digital experiences through motion, typography, and creative coding for companies and agencies around the world.</p>
+            </div>
+          <div className="p2_right">
+            <img src={img4} alt="idk" />
+            <h1>DIGITAL ART DIRECTOR INTERACTIVE DESIGNER CREATIVE DEVELOPER BASED IN AMSTERDAM, NL.</h1>
+          </div>
+        </div>
+        <div className="page3">
+            <h1>WEBISTE</h1>
+            <img src={img5} alt="stamp" />
+          </div>
+          <div className="page4">
+            <div className="p4_left">
+              <div className="p4_topContent">
+                <div className="p4_top-left">
+                  <h1>UPCOMING NEXT</h1>
+                  <h4>Fresh entry - A selected work from the latest digital releases.</h4>
+                  <h6><span>Tip!</span> Drag sideways to navigate</h6>
+                </div>
+                <div className="p4_top-right">
+                  <div className='top_right-img'>
+                    <img src={img6} alt="" />
+                  </div>
+                  <h4>UNEXPECTED TIME</h4>
+                  <p>Unexpected Time is a classic-furitistic gamification web experience showcasing the lost history & culture in a world dominated by the virtual reality. </p>
+                </div>
+              </div>
+              <img src={img7} alt="" />
+            </div>
+            <div className="p4_right">
+              <div className='p4_right-inner'>
+              <h1>Think, Create <span>DELIVER</span></h1>
+              <h5 className='first-letter'>A strong project is created by deep collaboration. I design, develop, and deliver websites that drive results and win awards. </h5>
+              <br /><br />
+              <h5>Like an artisan, I like to start from raw matter and give life to an iconic product that makes your brand stand out, starting from a Visual Strategy that guide the client’s vision to reality.</h5>
+              </div>
+              <div className="oval">
+                <h5>ALL WORK</h5>
+              </div>
+            </div>
+          </div>
+          <div className="page5"></div>
       </div>
     </>
   )
