42f27253c6b055df1fe7cb9773dc55f04f4e7397
chandrabosep <chandrabosep3112@gmail.com>
2023-07-03 00:01:26 +0530

    Added locomotive scrolla and completed website

--- src/pages/Marinda.jsx
+++ src/pages/Marinda.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import './Marinda.scss'
+import LocomotiveScroll from 'locomotive-scroll'
 import title from '../Assets/Title.svg'
 import menu from '../Assets/menu.svg'
 import img1 from '../Assets/img1.jpeg'
@@ -9,13 +10,18 @@ import img4 from '../Assets/img4.jpeg'
 import img5 from '../Assets/img5.png'
 import img6 from '../Assets/img6.WEBP'
 import img7 from '../Assets/img7.jpeg'
-
+import img8 from '../Assets/img8.jpeg'
+import img9 from '../Assets/img9.jpeg'
+import img10 from '../Assets/img10.jpeg'
+import img11 from '../Assets/img11.WEBP'
+import img12 from '../Assets/img12.png'
 import { gsap } from 'gsap'
 
 
 const Marinda = () => {
 
   const page1 = useRef();
+  const ref = useRef(null);
 
   useEffect(()=>{
     const animate = ()=>{
@@ -42,10 +48,16 @@ const Marinda = () => {
     return animate
   },[])
 
-
+  useEffect(()=>{
+    new LocomotiveScroll({
+      el: ref.current,
+      smooth: true,
+  });
+  },[])
+  
   return (
     <>
-      <div className="main">
+      <div className="main"  ref={ref}>
         <div className="page1" ref={page1}>
           <div className="nav">
             <h5>Amsterdam, NL</h5>
@@ -124,7 +136,106 @@ const Marinda = () => {
               </div>
             </div>
           </div>
-          <div className="page5"></div>
+          <div className="awards-banner">
+            <div className='awards_inner'>
+              <div className="award_1">
+                <div className='award_section-1'>
+                    <p>SITE OF THE DAY</p>
+                    <h5>AWARDS</h5>
+                  </div>
+                  <h5>9</h5>
+                </div>
+                <div className="award_2">
+                  <div className='award_section-2'>
+                    <p>SITE OF THE MONTH</p>
+                    <h5>WINNERS</h5>
+                  </div>
+                  <h5>1</h5>
+                </div>
+                <div className="award_3">
+                  <div className='award_section-3'>
+                    <p>FWA OF THE DAY</p>
+                    <h5>AWARDS</h5>
+                  </div>
+                  <h5>6</h5>
+                </div>
+                <div className="award_4">
+                  <div className='award_section-4'>
+                    <p>ACCLAIMED</p>
+                    <h5>MENTIONS</h5>
+                  </div>
+                  <h5>8</h5>
+                </div>
+              </div>
+            </div>
+            <div className="page5">
+              <div className="p5_top">
+                <div className="p5_top-left">
+                  <div className="p5_top-innerTop">
+                    <img src={img8} alt="" />
+                    <h1>THE</h1>
+                  </div>
+                  <div className="p5_top-innerBottom">
+                    <h1>PIXEL</h1>
+                  </div>
+                </div>
+                <div className="p5_top-right">
+                  <img src={img9} alt="" />
+                </div>
+              </div>
+              <div className='p5_center'>
+                <img src={img10} alt="" />
+                <h1>PERFECT</h1>
+              </div>
+              <div className="p5_bottom">
+                <h1>ARTISAN</h1>
+                <div className='p5_botom-right'>
+                  <p className='p5_para'>Over the past 5+ years, I've teamed up with high-profile clients and partners globally earning <a href="">mentions & awards </a>from digital platforms like The FWA,</p>
+                  <h5>AWWWARDS</h5>
+                  <p className='p5_para'>Communication Arts, Site Inspire, Behance, Codrops and many others.</p>
+                </div>
+              </div>            
+            </div>
+            <div className="hero page6">
+            <div className="Hero_Container-3">
+                <div className="container_img">
+                  <img src={img11} alt="" />
+                </div>
+                <h4>WOW CONCEPT <span>NEW</span></h4>
+                <p>WOW Concept is a the world’s first concept store based in Madrid revolutionizing retail with a dynamic & interactive shopping experience.</p>              
+                </div>
+              
+              <div className="Hero_Container-2">
+                <h2>ALL WORK!</h2>
+                <h3>A Featured selection <br/>the latest work- <br/>of the last years.</h3>
+                <h6><span>Tip!</span> Drag sideways to navigate</h6>
+              </div>
+              <div className="Hero_Container-1">
+                <div className="container_img">
+                  <img src={img1} alt="" />
+                </div>
+                <h4>AVRo | KO <span>NEW</span></h4>
+                
+                <p>AvroKO is an award-winning global design firm, established itself as a global leader in interior architecture for hospitality, restaurant and bars. </p>
+              </div>
+            </div>
+            <footer>
+              <div className="footer">
+                <div className="footer-left">
+                  <span>MARINDA &#xA9;</span>
+                  <img src={img12} alt="" />
+                  <p>Legal</p>
+                </div>
+                <div className="footer-right">
+                  <ul>
+                    <li className='l1'>TWITTER</li>
+                    <li>INSTAGRAM</li>
+                    <li>DRIBBLE</li>
+                    <li>BEHANCE</li>
+                  </ul>
+                </div>
+              </div>
+            </footer>
       </div>
     </>
   )
